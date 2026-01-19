45ff7c4268312f8dadb737f968a6081f60398906
Dave Ramos <dramos_240000000449@uic.edu.ph>
2026-01-20 00:45:53 +0800

    Delete resources directory

--- resources/views/components/edit-button.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ $href }}" class="inline-flex items-center space-x-1 bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200">
-    <i class="fas fa-edit"></i>
-    <span>{{ $label ?? 'Edit' }}</span>
-</a>
--- resources/views/components/view-button.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ $href }}" class="inline-flex items-center space-x-1 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition-colors duration-200">
-    <i class="fas fa-eye"></i>
-    <span>{{ $label ?? 'View' }}</span>
-</a>
